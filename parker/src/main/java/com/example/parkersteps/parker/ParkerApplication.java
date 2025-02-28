b8985a36d4c84ba0308108154018f2f3feeb147e
mariajoanna15 <joannajoyce111@gmail.com>
2025-02-28 11:00:58 +0530

    parker

---
+++ parker/src/main/java/com/example/parkersteps/parker/ParkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.parkersteps.parker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkerApplication.class, args);
+	}
+
+}
